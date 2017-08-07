a630df62e4d6ee2151b751ea0bb33b3bd3ab8bfa
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-08 01:06:27 +0900

    use foreach

--- test/components/component_box_test.js
+++ test/components/component_box_test.js
@@ -2,21 +2,21 @@ import { renderComponent, expect } from '../test_helper';
 import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox' , () => {
-  it('has a text area', () => {
-    const component = renderComponent(CommentBox);
+  let component;
 
+  beforeEach(() => {
+    component = renderComponent(CommentBox);
+  });
+
+  it('has a text area', () => {
     expect(component.find('textarea')).to.exist;
   });
 
   it('has a comment-test class', () => {
-    const component = renderComponent(CommentBox);
-
     expect(component.find('textarea')).to.have.class('comment-test');
   });
 
   it('has a button', () => {
-    const component = renderComponent(CommentBox);
-
     expect(component.find('button')).to.exist;
   });
 });
