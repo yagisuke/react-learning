6efcba29ad6ca4b5ccc386b51938ad0c6e7918f6
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-08 00:27:47 +0900

    add first test

---
+++ test/components/app_test.js
@@ -0,0 +1,10 @@
+import { renderComponent, expect } from '../test_helper';
+import App from '../../src/components/app';
+
+describe('App' , () => {
+  it('shows the correct text', () => {
+    const component = renderComponent(App);
+
+    expect(component).to.contain('React simple starter');
+  });
+});
