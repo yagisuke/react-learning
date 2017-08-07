aa26a6232ae1f956a4063dda9c029c0b7df1fae6
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-08 00:47:00 +0900

    check textarea

--- test/components/component_box_test.js
+++ test/components/component_box_test.js
@@ -3,6 +3,9 @@ import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox' , () => {
   it('has a text area', () => {
+    const component = renderComponent(CommentBox);
+
+    expect(component.find('textarea')).to.exist;
   });
 
   it('has a button', () => {
