67c1fbe2707c62aa2a501f1ce5c10aa5bf88daa5
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-08 00:39:18 +0900

    add comment box test

---
+++ test/components/component_box_test.js
@@ -0,0 +1,10 @@
+import { renderComponent, expect } from '../test_helper';
+import CommentBox from '../../src/components/comment_box';
+
+describe('CommentBox' , () => {
+  it('has a text area', () => {
+  });
+
+  it('has a button', () => {
+  });
+});
