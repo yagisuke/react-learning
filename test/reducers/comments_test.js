5294c37b7b1b2d8a3dbf4bdb62979050c2dccfeb
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-12 15:11:30 +0900

    add comment reducers test

---
+++ test/reducers/comments_test.js
@@ -0,0 +1,14 @@
+import { expect } from '../test_helper';
+import commentReducer from '../../src/reducers/comments';
+import { SAVE_COMMENT } from '../../src/actions/types';
+
+describe('Comments Reducer', () => {
+  it('handles action with unknown type', () => {
+    expect(commentReducer()).to.be.instanceof(Array);
+    expect(commentReducer()).to.be.eql([]);
+  });
+
+  it('handles action of type SAVE_COMMENT', () => {
+
+  });
+});
