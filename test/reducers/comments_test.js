3b5d35ee42c99675f54fa32313cb647729579ba8
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-12 15:35:45 +0900

    add comment reducers test

--- test/reducers/comments_test.js
+++ test/reducers/comments_test.js
@@ -4,11 +4,12 @@ import { SAVE_COMMENT } from '../../src/actions/types';
 
 describe('Comments Reducer', () => {
   it('handles action with unknown type', () => {
-    expect(commentReducer()).to.be.instanceof(Array);
-    expect(commentReducer()).to.be.eql([]);
+    expect(commentReducer(undefined, {})).to.be.instanceof(Array);
+    expect(commentReducer(undefined, {})).to.be.eql([]);
   });
 
   it('handles action of type SAVE_COMMENT', () => {
-
+    const action = { type: SAVE_COMMENT, payload: 'new comment' };
+    expect(commentReducer([], action)).to.eql(['new comment']);
   });
 });
