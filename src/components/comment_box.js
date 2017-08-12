039c23aa43cb0e1270e7529fc85aa9fffd4dbc39
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-12 15:09:59 +0900

    add actions test case

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -16,7 +16,7 @@ class CommentBox extends Component {
   handleSubmit(event) {
     event.preventDefault();
 
-    // this.props.saveComment(this.state.comment);
+    this.props.saveComment(this.state.comment);
     this.setState({ comment: '' });
   }
 
