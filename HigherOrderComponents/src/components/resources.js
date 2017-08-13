9cb32419eee036a876722511ce61402c55ff9917
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-13 14:01:04 +0900

    create resource components

---
+++ HigherOrderComponents/src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super Special Secret Recipe
+      <ul>
+        <li>1 cup sugar</li>
+        <li>1 cup pepper</li>
+        <li>1 cup salt</li>
+      </ul>
+    </div>
+  );
+};
