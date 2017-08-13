c7bc80912ac9a470e6c4afd765d0a52e9e1c3800
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-13 15:08:32 +0900

    no login user redirect func

--- HigherOrderComponents/src/components/require_authentication.js
+++ HigherOrderComponents/src/components/require_authentication.js
@@ -3,8 +3,18 @@ import { connect } from 'react-redux';
 
 export default function(ComposedComponent) {
   class Authentication extends Component {
+    static contextTypes = {
+      router: React.PropTypes.object
+    }
+
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
     render() {
-      console.log(this.props.authenticated);
+      console.log(this.context);
       return <ComposedComponent {...this.props} />
     }
   }
