7e7968ad8529bd8f95c6ec4d435e908da06b8b4e
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-14 15:48:17 +0900

    create server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
 // main starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+// app setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({type: '*/*'}));
+
+// server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('server listening on:', port);
