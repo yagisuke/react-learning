af0bbeb45ca24c43f362fef2a3ede91c879897ba
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-14 17:57:12 +0900

    creating user record

---
+++ server/controllers/authentication.js
@@ -0,0 +1,29 @@
+const User = require('../models/user');
+
+exports.signup = function(req, res, next) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // see if auser with the given email exists
+  User.findOne({email: email}, function(err, existingUser) {
+    if (err) { return next(err); }
+
+    // if a user with email does exist, return ann error
+    if (existingUser) {
+      return res.status(422).send({error: 'Email is in use'});
+    }
+
+    // if a user with does not exist, create and save user record
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save(function(err) {
+      if (err) { return next(err) }
+
+      // repond to request indicating the user was created
+      res.json({ success: true });
+    });
+  });
+}
