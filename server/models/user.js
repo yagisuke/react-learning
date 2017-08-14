f44900997f83020a69208b010fea782cc5e6f10e
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-14 18:08:32 +0900

    use crypt

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('brypt-nodejs');
 
 // define our model
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String
 });
+
+// on save hook, encrypt password
+userSchema.pre('save', function(next) {
+  const user = this;
+
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) { return next(err); }
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) { return next(err); }
+
+      user.password = hash;
+      next();
+    })
+  })
+})
 
 // Create the model class
 const ModelClass = mongoose.model('user', userSchema);
