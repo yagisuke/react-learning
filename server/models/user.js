adfaf14f04ddc049eab3f64135f2b38fb064c456
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-14 18:26:23 +0900

    misstype

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const bcrypt = require('brypt-nodejs');
+const bcrypt = require('bcrypt-nodejs');
 
 // define our model
 const userSchema = new Schema({
@@ -9,12 +9,16 @@ const userSchema = new Schema({
 });
 
 // on save hook, encrypt password
+// before saving a model, run this function
 userSchema.pre('save', function(next) {
+  // get access to the user model
   const user = this;
 
+  // generate a salt then run callback
   bcrypt.genSalt(10, function(err, salt) {
     if (err) { return next(err); }
 
+    // hash (encrypt) our password using the salt
     bcrypt.hash(user.password, salt, null, function(err, hash) {
       if (err) { return next(err); }
 
