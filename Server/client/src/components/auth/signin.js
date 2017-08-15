f2578eefa166a179e237c706088d43c5da70a209
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-15 23:55:27 +0900

    create sign in component

---
+++ Server/client/src/components/auth/signin.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { reactForm } from 'react-form';
+
+class Signin extends Component {
+  render() {
+    <form>
+      <fieldset className="form-group">
+        <label>Email:</label>
+        <input className="form-control" />
+      </fieldset>
+      <fieldset className="form-group">
+        <label>Password:</label>
+        <input className="form-control" />
+      </fieldset>
+      <button action="submit" className="btn btn-primary">Sign in</button>
+    </form>
+  }
+}
+
+export default reduxForm({
+  form: 'signin',
+  fields: ['email', 'password']
+})(Signin)
