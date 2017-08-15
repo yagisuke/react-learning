d863af779fdaa095109404cbb4a662d7d78200ef
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-15 22:01:24 +0900

    rename service dir

--- server/router.js
+++ server/router.js
@@ -1,5 +1,5 @@
 const Authentication = require('./controllers/authentication');
-const passportService = require('./service/passport');
+const passportService = require('./services/passport');
 const passport = require('passport');
 
 const requireAuth = passport.authenticate('jwt', { session: false });
