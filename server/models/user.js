5b7552b7f2a78a747bccc71e6f56b4db0986a5f5
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-15 22:30:53 +0900

    signin function

--- server/models/user.js
+++ server/models/user.js
@@ -24,9 +24,17 @@ userSchema.pre('save', function(next) {
 
       user.password = hash;
       next();
-    })
-  })
-})
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) { return callback(err); }
+
+    callback(null, isMatch);
+  });
+}
 
 // Create the model class
 const ModelClass = mongoose.model('user', userSchema);
