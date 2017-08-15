bfeb793a5608f71c60cba81e94754ce76f626c49
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-15 23:59:28 +0900

    modify signin component

--- Server/client/src/components/auth/signin.js
+++ Server/client/src/components/auth/signin.js
@@ -3,17 +3,19 @@ import { reactForm } from 'react-form';
 
 class Signin extends Component {
   render() {
-    <form>
-      <fieldset className="form-group">
-        <label>Email:</label>
-        <input className="form-control" />
-      </fieldset>
-      <fieldset className="form-group">
-        <label>Password:</label>
-        <input className="form-control" />
-      </fieldset>
-      <button action="submit" className="btn btn-primary">Sign in</button>
-    </form>
+    return (
+      <form>
+        <fieldset className="form-group">
+          <label>Email:</label>
+          <input className="form-control" />
+        </fieldset>
+        <fieldset className="form-group">
+          <label>Password:</label>
+          <input className="form-control" />
+        </fieldset>
+        <button action="submit" className="btn btn-primary">Sign in</button>
+      </form>
+    );
   }
 }
 
