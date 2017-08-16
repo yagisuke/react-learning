06032af42fbd65fdddf69832b571850f4cf46bf1
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-16 17:27:31 +0900

    setup auth reducer

--- Server/client/src/actions/index.js
+++ Server/client/src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
+import { AUTH_USER } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -10,6 +11,7 @@ export function signinUser({ email, password }){
       .then(response => {
         // if request is good...
         // - update state to indicate user is authenticated
+        dispatch({ tpe: AUTH_USER });
         // - save the jwt token
         // - redirect to the route '/feature'
         browserHistory.push('/feature');
