12c1bf78ed259005b7f579b537fc02807d5c908c
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-16 17:13:11 +0900

    create sign in success pattern

--- Server/client/src/actions/index.js
+++ Server/client/src/actions/index.js
@@ -1,18 +1,22 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
 
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }){
   return function(dispatch) {
     // submit email/password to the server
-    axios.post(`${ROOT_URL}/signin`, { email, password });
-
-    // if request is good...
-    // - update state to indicate user is authenticated
-    // - save the jwt token
-    // - redirect to the route '/feature'
-
-    // if request is bad...
-    // -show an error to the user
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => {
+        // if request is good...
+        // - update state to indicate user is authenticated
+        // - save the jwt token
+        // - redirect to the route '/feature'
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
+        // if request is bad...
+        // -show an error to the user
+      });
   }
 }
