e08e5f5f1e8574cd53eb1c3250136718eb7afdff
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-16 16:35:43 +0900

    sign in logic memo

---
+++ Server/client/src/actions/index.js
@@ -0,0 +1,11 @@
+export function signinUser({ email, password }){
+  // submit email/password to the server
+
+  // if request is good...
+  // - update state to indicate user is authenticated
+  // - save the jwt token
+  // - redirect to the route '/feature'
+
+  // if request is bad...
+  // -show an error to the user
+}
