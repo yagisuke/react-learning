63245fd49529544107f35341cb0d0baff020ab64
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-16 17:04:19 +0900

    sign in submit

--- Server/client/src/actions/index.js
+++ Server/client/src/actions/index.js
@@ -1,11 +1,18 @@
+import axios from 'axios';
+
+const ROOT_URL = 'http://localhost:3090';
+
 export function signinUser({ email, password }){
-  // submit email/password to the server
+  return function(dispatch) {
+    // submit email/password to the server
+    axios.post(`${ROOT_URL}/signin`, { email, password });
 
-  // if request is good...
-  // - update state to indicate user is authenticated
-  // - save the jwt token
-  // - redirect to the route '/feature'
+    // if request is good...
+    // - update state to indicate user is authenticated
+    // - save the jwt token
+    // - redirect to the route '/feature'
 
-  // if request is bad...
-  // -show an error to the user
+    // if request is bad...
+    // -show an error to the user
+  }
 }
--- server/index.js
+++ server/index.js
@@ -6,12 +6,14 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // db setup
 mongoose.connect('mongodb://localhost:auth/auth');
 
 // app setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({type: '*/*'}));
 router(app);
 
