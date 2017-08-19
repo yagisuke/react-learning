9ebe8ddfce1a4d708fe30e118f78912eaadc7b55
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-19 16:58:43 +0900

    add signing, signup, signout links

--- Server/client/src/components/header.js
+++ Server/client/src/components/header.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 class Header extends Component {
+  renderLinks() {
+    if (this.props.authenticated) {
+      return <li className="nav-item">
+          <Link to="/signout" className="nav-link">Sing Out</Link>
+        </li>
+    } else {
+      return [
+        <li className="nav-item" key={1}>
+          <Link to="/signin" className="nav-link">Sing In</Link>
+        </li>,
+        <li className="nav-item" key={2}>
+          <Link to="/signup" className="nav-link">Sing Up</Link>
+        </li>
+      ];
+    }
+  }
+
   render() {
     return (
       <nav className="navbar navbar-light">
+        <Link to="/" className="navbar-brand">Redux Auth</Link>
         <ul className="nav navbar-nav">
-          <li className="nav-item">
-            Sign in
-          </li>
+          {this.renderLinks()}
         </ul>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return {
+    authenticated: state.auth.authenticated
+  }
+}
+
+export default connect()(Header);
