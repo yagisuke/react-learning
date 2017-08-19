0f849b7d01b3fef42b5cea33d2f55e51de615a33
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-19 17:20:01 +0900

    create signup component

--- Server/client/src/index.js
+++ Server/client/src/index.js
@@ -7,7 +7,8 @@ import reduxThunk from 'redux-thunk';
 
 import App from './components/app';
 import Signin from './components/auth/signin';
-import SignOut from './components/auth/signout';
+import Signup from './components/auth/signup';
+import Signout from './components/auth/signout';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
@@ -17,7 +18,8 @@ ReactDOM.render(
     <Router history={ browserHistory }>
       <Route path="/" component={App}>
         <Route path="signin" component={Signin} />
-        <Route path="signout" component={SignOut} />
+        <Route path="signup" component={Signup} />
+        <Route path="signout" component={Signout} />
       </Route>
     </Router>
   </Provider>
