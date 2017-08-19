2f287c51478c0c861deef084a83d8a465dda4503
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-08-19 19:53:37 +0900

    automatically authenticating user

--- Server/client/src/index.js
+++ Server/client/src/index.js
@@ -4,20 +4,29 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import reduxThunk from 'redux-thunk';
+import { AUTH_USER } from './actions/types';
 
 import App from './components/app';
 import Signin from './components/auth/signin';
 import Signup from './components/auth/signup';
 import Signout from './components/auth/signout';
 import Feature from './components/feature';
-import RequireAuth from './components/auth/require_auth'
-import Welcome from './components/welcome'
+import RequireAuth from './components/auth/require_auth';
+import Welcome from './components/welcome';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+// if we have a token, consider ther user to be signed in
+if (token) {
+  // we need to update aplication state
+  store.dispatch({ type: AUTH_USER })
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={ browserHistory }>
       <Route path="/" component={App}>
         <IndexRoute component={Welcome} />
